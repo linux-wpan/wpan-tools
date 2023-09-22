6af63e901cb88c4881f4a86859523a3ee10d3217
Stefan Schmidt <stefan@datenfreihafen.org>
2023-09-22 17:23:51 +0200

    misc: switch remaining files to SPDX header for license and copyright
    Reviewed-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Stefan Schmidt <stefan@datenfreihafen.org>

--- autogen.sh
+++ autogen.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -e
 
+# SPDX-FileCopyrightText: 2014 Alexander Aring <alex.aring@gmail.com>
+#
+# SPDX-License-Identifier: ISC
+
 if [ -f .git/hooks/pre-commit.sample -a ! -f .git/hooks/pre-commit ] ; then
         cp -p .git/hooks/pre-commit.sample .git/hooks/pre-commit && \
         chmod +x .git/hooks/pre-commit && \
