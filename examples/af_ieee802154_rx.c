936135a7f77424d740fb93307827065e52f7ddf5
Stefan Schmidt <stefan@datenfreihafen.org>
2023-09-22 16:57:54 +0200

    examples: switch files to SPDX header for license and copyright
    Reviewed-by: Miquel Raynal <miquel.raynal@bootlin.com>
    Signed-off-by: Stefan Schmidt <stefan@datenfreihafen.org>

--- examples/af_ieee802154_rx.c
+++ examples/af_ieee802154_rx.c
@@ -1,23 +1,8 @@
-/*
- * IEEE 802.15.4 socket example
- *
- * Copyright (C) 2016 Samsung Electronics Co., Ltd.
- *
- * Author: Stefan Schmidt <stefan@osg.samsung.com>
- *
- * Permission to use, copy, modify, and/or distribute this software for any
- * purpose with or without fee is hereby granted, provided that the above
- * copyright notice and this permission notice appear in all copies.
- *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
- * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
- * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
- * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
- * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- */
+// SPDX-FileCopyrightText: 2016 Samsung Electronics Co., Ltd.
+//
+// SPDX-License-Identifier: ISC
 
+/* IEEE 802.15.4 socket example */
 /* gcc af_ieee802154_rx.c -o af_ieee802154_rx */
 
 #include <sys/types.h>
